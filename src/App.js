02e559ffef5b0c1548a5fe52d9497ec3da43e764
dasha akhten <dasha.akhten@gmail.com>
2020-04-11 23:09:40 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>
+       foodversity
+     </h1>
+     <p>stop creepin... there's nothing here yet </p>
+    </div>
+  );
+}
+
+export default App;
+
+//open terminal is control tilde
